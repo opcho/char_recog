2ea80175d5f5d2742cdfceaa2803d30a0eb04001
opcho <36229243+opcho@users.noreply.github.com>
2019-02-18 20:06:21 +0900

    Update custom_datasets.py

--- custom_datasets.py
+++ custom_datasets.py
@@ -6,14 +6,17 @@ from PIL import Image
 
 class Chars74k(Dataset):
 
-    def __init__(self, csv_file, root_dir,CLASSES, target_transform,transform=None,):
+    def __init__(self, csv_file, root_dir,CLASSES, target_transform,transform=None,transform2=None):
 
         self.data_info = pd.read_csv(csv_file, header=None)
         self.root_dir = root_dir
         self.CLASSES=CLASSES
         self.transform = transform
+        self.transform2 = transform2
         self.target_transform = target_transform
         assert os.path.isfile(csv_file)
+        for i in range(len(self.data_info.index)):
+            assert os.path.exists(os.path.join(self.root_dir, self.data_info.iloc[i, 0]))
 
     def __len__(self):
         return len(self.data_info.index)
@@ -25,10 +28,14 @@ class Chars74k(Dataset):
         # label to index number
         label=self.data_info.iloc[idx, 1]
 
-        if self.transform:
-            image = self.transform(image)
+        image1 = self.transform(image)
+        image2 = self.transform2(image)
 
         if self.target_transform:
             label = self.target_transform(self.CLASSES,label)
 
-        return image,label
+        return image1,image2,label
+
+if __name__ == "__main__":
+    # Call dataset
+    pass
